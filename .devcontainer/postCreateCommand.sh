04a2dec5b1e2812c553ec528773f1414b02c377b
Jun-Hong Cheng <qwe854896@gmail.com>
2023-02-22 11:18:31 +0800

    chore: modify shebang of the script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
+# git config
 git config --global user.email "qwe854896@gmail.com"
 git config --global user.name "Jun-Hong Cheng"
 git config --global core.editor "code --wait"
+
+# node_modules
 yarn install
