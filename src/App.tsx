7a0076e96468d20e658442a9207107d11b3caadd
Jun-Hong Cheng <qwe854896@gmail.com>
2023-03-02 07:52:36 +0000

    refactor: adding router in `App.tsx`
    - move original page to `src/pages/ViteReact`

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
-import { VITE_APP_TITLE, VITE_TITLE_VISIBLE } from '@c/constant';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const title: string = VITE_APP_TITLE;
-  const showTitle: boolean = VITE_TITLE_VISIBLE;
+import ViteReact from '@/pages/ViteReact';
 
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      {showTitle ? <h1>{title}</h1> : null}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        {count > 0 ? (
-          <p>
-            <code>The count is now: {count}</code>
-          </p>
-        ) : null}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <Suspense fallback={<span>loading</span>}>
+      <Router>
+        <Routes>
+          <Route key="/vite-react" path="/" element={<ViteReact />}></Route>
+        </Routes>
+      </Router>
+    </Suspense>
   );
-}
+};
 
 export default App;
---
+++ src/pages/ViteReact/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import reactLogo from '@/assets/react.svg';
+import { VITE_APP_TITLE, VITE_TITLE_VISIBLE } from '@c/constant';
+
+const ViteReact = () => {
+  const [count, setCount] = useState(0);
+  const title: string = VITE_APP_TITLE;
+  const showTitle: boolean = VITE_TITLE_VISIBLE;
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      {showTitle ? <h1>{title}</h1> : null}
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        {count > 0 ? (
+          <p>
+            <code>The count is now: {count}</code>
+          </p>
+        ) : null}
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+};
+
+export default ViteReact;
