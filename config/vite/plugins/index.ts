3c0a344345f9e9d915ccf79c416b7b8b2d2994fd
Jun-Hong Cheng <qwe854896@gmail.com>
2023-03-06 20:09:40 +0800

    fix: vitest config issue
    - mergeConfig cannot handle conditional config

--- config/vite/plugins/index.ts
+++ config/vite/plugins/index.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 import configMockPlugin from './mock';
 
-export function createVitePlugins(mode: string, isBuild: boolean) {
+export function createVitePlugins(isBuild: boolean) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [react(), configMockPlugin(isBuild)];
 
   return vitePlugins;
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,33 @@
 import path from 'path';
-import type { ConfigEnv, UserConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vite';
 
 import { PORT } from './config/constant';
 import { createProxy } from './config/vite/proxy';
 import { createVitePlugins } from './config/vite/plugins';
 
-// https://vitejs.dev/config/
-export default ({ command, mode }: ConfigEnv): UserConfig => {
+export const commonConfig = defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@c', replacement: path.resolve(__dirname, 'config') },
+    ],
+  },
+  server: {
+    host: true,
+    port: PORT,
+    proxy: createProxy(),
+  },
+});
+
+export default defineConfig(({ command, mode }) => {
   const isBuild = command === 'build';
 
-  return {
-    plugins: createVitePlugins(mode, isBuild),
-    resolve: {
-      alias: [
-        { find: '@', replacement: path.resolve(__dirname, 'src') },
-        { find: '@c', replacement: path.resolve(__dirname, 'config') },
-      ],
-    },
-    server: {
-      host: true,
-      port: PORT,
-      proxy: createProxy(),
-    },
-    preview: {
-      host: true,
-      port: PORT,
-      proxy: createProxy(),
-    },
-  };
-};
+  console.log(command, mode);
+
+  return mergeConfig(
+    commonConfig,
+    defineConfig({
+      plugins: createVitePlugins(isBuild),
+    }),
+  );
+});
